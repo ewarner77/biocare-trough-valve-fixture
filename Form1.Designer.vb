687086191ac01876699eae4fc5320cb818a01669
ewarner <eric@warner-consulting.com>
2019-11-08 05:47:41 -0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,302 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button15 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.Button14 = New System.Windows.Forms.Button()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.GroupBox4.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(97, 506)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(77, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(82, 461)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(77, 25)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Label2"
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(34, 37)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(162, 45)
+        Me.Button4.TabIndex = 5
+        Me.Button4.Text = "initialize"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button15)
+        Me.GroupBox1.Controls.Add(Me.Button10)
+        Me.GroupBox1.Controls.Add(Me.Button11)
+        Me.GroupBox1.Controls.Add(Me.Button12)
+        Me.GroupBox1.Controls.Add(Me.Button13)
+        Me.GroupBox1.Controls.Add(Me.Button14)
+        Me.GroupBox1.Location = New System.Drawing.Point(33, 52)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(276, 384)
+        Me.GroupBox1.TabIndex = 6
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "to trough inputs"
+        '
+        'Button15
+        '
+        Me.Button15.Location = New System.Drawing.Point(62, 37)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(162, 45)
+        Me.Button15.TabIndex = 16
+        Me.Button15.Text = "initialize"
+        Me.Button15.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(62, 301)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(59, 50)
+        Me.Button10.TabIndex = 15
+        Me.Button10.Text = "5"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(62, 251)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(59, 50)
+        Me.Button11.TabIndex = 14
+        Me.Button11.Text = "4"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Button12
+        '
+        Me.Button12.Location = New System.Drawing.Point(62, 201)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(59, 50)
+        Me.Button12.TabIndex = 13
+        Me.Button12.Text = "3"
+        Me.Button12.UseVisualStyleBackColor = True
+        '
+        'Button13
+        '
+        Me.Button13.Location = New System.Drawing.Point(62, 151)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(162, 50)
+        Me.Button13.TabIndex = 12
+        Me.Button13.Text = "2 - cold"
+        Me.Button13.UseVisualStyleBackColor = True
+        '
+        'Button14
+        '
+        Me.Button14.Location = New System.Drawing.Point(62, 101)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(162, 50)
+        Me.Button14.TabIndex = 11
+        Me.Button14.Text = "1 - hot"
+        Me.Button14.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Button9)
+        Me.GroupBox2.Controls.Add(Me.Button8)
+        Me.GroupBox2.Controls.Add(Me.Button7)
+        Me.GroupBox2.Controls.Add(Me.Button6)
+        Me.GroupBox2.Controls.Add(Me.Button5)
+        Me.GroupBox2.Controls.Add(Me.Button4)
+        Me.GroupBox2.Location = New System.Drawing.Point(354, 52)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(256, 384)
+        Me.GroupBox2.TabIndex = 7
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "from trough outputs"
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(34, 301)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(59, 50)
+        Me.Button9.TabIndex = 10
+        Me.Button9.Text = "5"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(34, 251)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(59, 50)
+        Me.Button8.TabIndex = 9
+        Me.Button8.Text = "4"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(34, 201)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(59, 50)
+        Me.Button7.TabIndex = 8
+        Me.Button7.Text = "3"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(34, 151)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(162, 50)
+        Me.Button6.TabIndex = 7
+        Me.Button6.Text = "2 - cold"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(34, 101)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(162, 50)
+        Me.Button5.TabIndex = 6
+        Me.Button5.Text = "1 - hot"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.GroupBox2)
+        Me.GroupBox3.Controls.Add(Me.GroupBox1)
+        Me.GroupBox3.Location = New System.Drawing.Point(994, 68)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(641, 474)
+        Me.GroupBox3.TabIndex = 8
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "manual full control"
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Controls.Add(Me.Button3)
+        Me.GroupBox4.Controls.Add(Me.Button2)
+        Me.GroupBox4.Controls.Add(Me.Button1)
+        Me.GroupBox4.Location = New System.Drawing.Point(46, 32)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(456, 377)
+        Me.GroupBox4.TabIndex = 9
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "running test"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(41, 74)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(231, 48)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "from hot to hot"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(44, 141)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(227, 59)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "from cold to cold"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(265, 288)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(150, 67)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1686, 805)
+        Me.Controls.Add(Me.GroupBox4)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "valve positioning"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox4.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button4 As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents GroupBox4 As GroupBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,132 @@
+﻿Imports System.Text
+Public Class Form1
+    Declare Sub Sleep Lib "kernel32" Alias "Sleep" _
+   (ByVal dwMilliseconds As Long)
+    Public Property Encoding As System.Text.Encoding
+
+    Private Sub Wait(ByVal DurationMS As Long)
+        Dim EndTime As Long
+        Dim counting As Long
+        EndTime = Environment.TickCount + DurationMS
+        Do While EndTime > Environment.TickCount
+            counting = (EndTime - Environment.TickCount) / 1000
+            Label1.Text = Format(counting, "00")
+            Application.DoEvents()
+            Threading.Thread.Sleep(100)
+        Loop
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Open_Serial()
+    End Sub
+
+    Private Sub Open_Serial()
+        'Dim utf8 As System.Text.Encoding = Encoding.UTF8
+        SerialPort1.PortName = "COM4"
+        SerialPort1.BaudRate = 9600
+        SerialPort1.Parity = IO.Ports.Parity.None
+        SerialPort1.StopBits = IO.Ports.StopBits.One
+        SerialPort1.DataBits = 8
+        SerialPort1.Handshake = IO.Ports.Handshake.None
+        'SerialPort1.Encoding = Encoding.UTF8
+        SerialPort1.Open()
+    End Sub
+
+    Private Function Send_String(ByVal sendstr As String) As String
+        Dim holdstr As String
+        'Dim utf8 As New System.Text.UTF8Encoding()
+        holdstr = ""
+        If SerialPort1.IsOpen Then
+            Dim stringback As String
+            SerialPort1.Write(sendstr & Chr(10) & Chr(13))
+            Wait(50)
+            stringback = SerialPort1.ReadExisting()
+            Send_String = stringback
+            For i = 1 To Len(stringback)
+                holdstr = holdstr & Asc(Mid(stringback, i, 1)) & ":"
+            Next
+        Else
+            Send_String = ""
+        End If
+    End Function
+    Private Sub valve_position(vno As Integer, pos As Integer)
+        Dim posno As Integer
+        Dim outstr As String
+        'If pos = 1 Then posno = 0
+        'If pos = 2 Then posno = 72
+        'If pos = 3 Then posno = 144
+        'If pos = 4 Then posno = 216
+        'If pos = 5 Then posno = 288
+        posno = (pos - 1) * 72
+        outstr = "/" & Format(vno, "0") & "h29" & Format(posno, "000") & "R"
+        Label2.Text = Send_String(outstr)
+    End Sub
+    Private Function query_valve(vno As Integer)
+        Dim outstr As String
+        outstr = "/" & Format(vno, "0") & "?25000R"
+
+    End Function
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Label2.Text = Send_String("/2ZR")
+    End Sub
+
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+        Label2.Text = Send_String("/1ZR")
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        valve_position(2, 1)
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        valve_position(2, 2)
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        valve_position(2, 3)
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        valve_position(2, 4)
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        valve_position(2, 5)
+    End Sub
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        valve_position(1, 1)
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        valve_position(1, 2)
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        valve_position(1, 3)
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        valve_position(1, 4)
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        valve_position(1, 5)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        valve_position(2, 1)
+        Wait(50)
+        valve_position(1, 1)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        valve_position(2, 2)
+        Wait(50)
+        valve_position(1, 2)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Label2.Text = Send_String("/1?25000R")
+    End Sub
+End Class
